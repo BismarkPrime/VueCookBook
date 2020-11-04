159477dd45218977abb9e4f0fa36869d055b8bb5
Adam J <17awesomead@gmail.com>
2020-11-04 16:55:21 -0700

    Reset Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -31,21 +31,23 @@ export default {
     };
   },
   computed: {
-    products() {
+    async products() {
       var myUrl = "https://cors-anywhere.herokuapp.com";
       myUrl += "/http://www.recipepuppy.com/api/?i=" + this.searchText;
-      useJSON();
-        async function fetchRecipeJSON() {
-        const response = await fetch(myUrl, {mode: 'cors'});
-        let recipeJSON = await response.json();
-        console.log(recipeJSON);
-        return recipeJSON;
-      }
+
       async function useJSON() {
         let passedHalf = false;
         let json = await fetchRecipeJSON();
         console.log(json);
       }
+      async function fetchRecipeJSON() {
+        const response = await fetch(myUrl, {mode: 'cors'});
+        let recipeJSON = await response.json();
+        console.log(recipeJSON);
+        return recipeJSON;
+      }
+
+      useJSON();
     },
     products_old() {
       return this.$root.$data.products.filter(
